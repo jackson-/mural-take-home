58d72f92ff8f087c53acf0c76a5d0fad77867328
Devin Jackson <devinjackson@Devins-MacBook-Pro.local>
2022-12-28 01:34:19 -0500

    safe functions in comments

--- web3-dashboard/src/pages/Multisig.js
+++ web3-dashboard/src/pages/Multisig.js
@@ -1,4 +1,6 @@
 import React, {useState} from "react";
+// import Safe, { SafeFactory, SafeAccountConfig } from '@gnosis.pm/safe-core-sdk'
+import {ethers} from "ethers";
 
 import "./Multisig.css"; 
 
@@ -9,16 +11,33 @@ const Multisig = () => {
     
     const nickNames = {}
 
+    // const createWallet = (e) => {
+    //     e.preventDefault();
+    //     const ownerAddresses = e.target[0].value.split(',');
+    //     const threshold = e.target[1].value;
+    //     try{
+    //         setConfirmationThreshold(threshold);
+    //         setOwners(ownerAddresses);
+    //         const safeAccountConfig = new SafeAccountConfig(ownerAddresses, threshold);
+    //         const safeFactory = new SafeFactory.create({ ethAdapter });
+    //         const safe = safeFactory.deploySafe(safeAccountConfig);
+    //         setSafe(safe);
+    //     } catch(error) {
+    //         console.log(error);
+    //         debugger;
+    //     }
+    // }
+
 
     // Render methods
 
-    const renderSetupForm = (safe) => (
-        <div className="multisig-create-form-container">
+    const renderWalletForm = (safe) => (
+        <div className="multisig-form-container">
             {safe ? <p>Edit Safe:</p> : <p>Create Safe</p>}
             <form className="multisig-create-form">
                 <input type="text" placeholder="Owner addresses seperated by comma"></input>
                 <input type="number" placeholder="Confirmation threshold"></input>
-                <button>Submit</button>
+                {/* <button onClick={createWallet}>Submit</button> */}
             </form>
         </div>
     )
@@ -63,7 +82,7 @@ const Multisig = () => {
         <div className="multisig-container">
             <h1>Multisig Wallet</h1>
             {safe && <p>Confirmation Threshold: {confirmationThreshold}</p>}
-            {renderSetupForm(safe)}
+            {renderWalletForm(safe)}
             {safe && renderOwnerTable()}
         </div>
     )
